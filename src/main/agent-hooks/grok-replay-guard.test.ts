paidaxingyo666/Manta

fix(grok): stop replayed Claude/Cursor hooks reporting Grok panes as Claude (#20507)
* fix(grok): stop replayed Claude/Cursor hooks reporting Grok panes as Claude

Grok's hook discovery reads ~/.claude/settings.json (and the Cursor equivalent)
for vendor compatibility, and that is on by default. So inside every Grok pane
Orca's managed Claude hook fires in addition to Orca's managed Grok hook, and
both POST the same Grok envelope. The Claude-routed copy lands last and wins, so
the pane's agent type is resolved from the POST route as "claude" and no
Grok-specific normalization runs for it.

Guard the managed Claude and Cursor scripts on GROK_HOOK_EVENT, which Grok's hook
runner stamps into every hook subprocess it spawns — including replayed vendor
configs — after any user-supplied environment, so a hook cannot spoof it. This
mirrors the existing DEVIN_PROJECT_DIR guard in the same script, which solves the
identical problem for another agent that imports Claude hooks.

Placement is load-bearing: the guard sits after the stdin capture, so Grok's
writer never blocks, and before both the spool write and the HTTP POST, so a
replayed event cannot leave a spool entry that replays later. The Windows
variants jump to the stdin-drain label rather than exiting, because abandoning
stdin there hangs the writer.

The guard is scoped to agent === 'claude'; OpenClaude reuses ClaudeHookService
with its own settings file, which Grok does not replay, so it is unaffected.

Verified live against Grok 1.0.25 in a dev instance: the pane's reported agent
type goes from "claude" to "grok" on every turn-end, including the hidden
follow-up turns Grok runs when background work finishes.

The guard pushed hook-service.ts past the 300-line cap, so the script builder
moves to a sibling hook-script.ts. That mirrors the existing split under
src/main/cursor/, where the service owns install/status and the script module
owns script text.

* fix(agent-hooks): preserve Windows background worker stdin co

src/main/agent-hooks/grok-replay-guard.test.ts
@@ -1,0 +1,136 @@
+import { spawnSync } from 'node:child_process'
+import { chmodSync, existsSync, mkdtempSync, rmSync, writeFileSync } from 'node:fs'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { describe, expect, it, vi } from 'vitest'
+
+vi.mock('electron', () => ({
+  app: {
+    getPath: () => '/tmp/userData'
+  }
+}))
+
+import { getManagedScript as getClaudeManagedScript } from '../claude/hook-service'
+import { getManagedScript as getCursorManagedScript } from '../cursor/hook-script'
+
+const POSIX_GROK_GUARD = 'if [ -n "$GROK_HOOK_EVENT" ]; then'
+const WINDOWS_GROK_GUARD = 'if not "%GROK_HOOK_EVENT%"=="" goto :manta_agent_hook_drain_stdin'
+const CLAUDE_SCRIPT_OPTIONS = {
+  skipWhenDevinImportsClaude: true,
+  skipWhenGrokImportsClaude: true
+}
+
+function withPlatform<T>(platform: NodeJS.Platform, run: () => T): T {
+  const descriptor = Object.getOwnPropertyDescriptor(process, 'platform')!
+  Object.defineProperty(process, 'platform', { value: platform, configurable: true })
+  try {
+    return run()
+  } finally {
+    Object.defineProperty(process, 'platform', descriptor)
+  }
+}
+
+function expectGuardBeforeTransport(
+  script: string,
+  guard: string,
+  response: string,
+  spool?: string
+): void {
+  const guardIndex = script.indexOf(guard)
+  expect(guardIndex).toBeGreaterThan(script.indexOf(response))
+  expect(guardIndex).toBeLessThan(script.indexOf('curl'))
+  if (spool) {
+    expect(guardIndex).toBeLessThan(script.indexOf(spool))
+  }
+}
+
+function runPosixHook(
+  script: string,
+  grokHookEvent: string
+): {
+  curlCalled: boolean
+  stdout: string
+} {
+  const dir = mkdtempSync(join(tmpdir(), 'orca-grok-replay-'))
+  const scriptPath = join(dir, 'hook.sh')
+  const curlPath = join(dir, 'curl')
+  const curlLog = join(dir, 'curl.log')
+  try {
+    writeFileSync(scriptPath, script)
+    writeFileSync(
+      curlPath,
+      '#!/bin/sh\n{ command -p cat 2>/dev/null || cat; } >/dev/null\nprintf "called\\n" >> "$CURL_LOG"\n'
+    )
+    chmodSync(scriptPath, 0o755)
+    chmodSync(curlPath, 0o755)
+
+    const result = spawnSync('/bin/sh', [scriptPath], {
+      encoding: 'utf8',
+      input: '{"hook_event_name":"Stop"}',
+      env: {
+        ...process.env,
+        PATH: `${dir}:${process.env.PATH ?? ''}`,
+        CURL_LOG: curlLog,
+        GROK_HOOK_EVENT: grokHookEvent,
+        MANTA_AGENT_HOOK_ENDPOINT: '',
+        MANTA_AGENT_HOOK_PORT: '1234',
+        MANTA_AGENT_HOOK_TOKEN: 'token',
+        MANTA_PANE_KEY: 'tab:leaf'
+      }
+    })
+
+    expect(result.error).toBeUndefined()
+    expect(result.status).toBe(0)
+    return { curlCalled: existsSync(curlLog), stdout: result.stdout }
+  } finally {
+    rmSync(dir, { recursive: true, force: true })
+  }
+}
+
+describe('Grok vendor hook replay guard', () => {
+  it('precedes spooling and HTTP in the generated POSIX Claude and Cursor scripts', () => {
+    const claude = getClaudeManagedScript('posix', CLAUDE_SCRIPT_OPTIONS)
+    const cursor = getCursorManagedScript('posix')
+
+    expectGuardBeforeTransport(claude, POSIX_GROK_GUARD, 'printf "{}\\n"', 'spool_hook_event')
+    expectGuardBeforeTransport(cursor, POSIX_GROK_GUARD, 'printf "{}\\n"', 'spool_hook_event')
+  })
+
+  it('precedes HTTP while preserving fail-open output in generated Windows scripts', () => {
+    const { claude, cursor } = withPlatform('win32', () => ({
+      claude: getClaudeManagedScript('local', CLAUDE_SCRIPT_OPTIONS),
+      cursor: getCursorManagedScript('local')
+    }))
+
+    expectGuardBeforeTransport(claude, WINDOWS_GROK_GUARD, 'echo {}')
+    expectGuardBeforeTransport(cursor, WINDOWS_GROK_GUARD, '(echo {})')
+    const backgroundWorkerGuardIndex = claude.indexOf('CLAUDE_JOB_DIR')
+    expect(backgroundWorkerGuardIndex).toBeGreaterThan(-1)
+    expect(backgroundWorkerGuardIndex).toBeLessThan(claude.indexOf(WINDOWS_GROK_GUARD))
+  })
+
+  it.skipIf(process.platform === 'win32')(
+    'drops Grok-replayed hooks without suppressing their protocol response',
+    () => {
+      for (const script of [
+        getClaudeManagedScript('posix', CLAUDE_SCRIPT_OPTIONS),
+        getCursorManagedScript('posix')
+      ]) {
+        const result = runPosixHook(script, 'Stop')
+        expect(result.curlCalled).toBe(false)
+        expect(result.stdout).toBe('{}\n')
+      }
+    }
+  )
+
+  it.skipIf(process.platform === 'win32')('leaves non-Grok hook delivery unchanged', () => {
+    for (const script of [
+      getClaudeManagedScript('posix', CLAUDE_SCRIPT_OPTIONS),
+      getCursorManagedScript('posix')
+    ]) {
+      const result = runPosixHook(script, '')
+      expect(result.curlCalled).toBe(true)
+      expect(result.stdout).toBe('{}\n')
+    }
+  })
+})

0ics-srls/orca

(cherry picked from commit e944e7653787b9a70c095437374eb71dc90363dd)

src/main/agent-hooks/grok-replay-guard.test.ts
@@ -1,0 +1,136 @@
+import { spawnSync } from 'node:child_process'
+import { chmodSync, existsSync, mkdtempSync, rmSync, writeFileSync } from 'node:fs'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { describe, expect, it, vi } from 'vitest'
+
+vi.mock('electron', () => ({
+  app: {
+    getPath: () => '/tmp/userData'
+  }
+}))
+
+import { getManagedScript as getClaudeManagedScript } from '../claude/hook-service'
+import { getManagedScript as getCursorManagedScript } from '../cursor/hook-script'
+
+const POSIX_GROK_GUARD = 'if [ -n "$GROK_HOOK_EVENT" ]; then'
+const WINDOWS_GROK_GUARD = 'if not "%GROK_HOOK_EVENT%"=="" goto :orca_agent_hook_drain_stdin'
+const CLAUDE_SCRIPT_OPTIONS = {
+  skipWhenDevinImportsClaude: true,
+  skipWhenGrokImportsClaude: true
+}
+
+function withPlatform<T>(platform: NodeJS.Platform, run: () => T): T {
+  const descriptor = Object.getOwnPropertyDescriptor(process, 'platform')!
+  Object.defineProperty(process, 'platform', { value: platform, configurable: true })
+  try {
+    return run()
+  } finally {
+    Object.defineProperty(process, 'platform', descriptor)
+  }
+}
+
+function expectGuardBeforeTransport(
+  script: string,
+  guard: string,
+  response: string,
+  spool?: string
+): void {
+  const guardIndex = script.indexOf(guard)
+  expect(guardIndex).toBeGreaterThan(script.indexOf(response))
+  expect(guardIndex).toBeLessThan(script.indexOf('curl'))
+  if (spool) {
+    expect(guardIndex).toBeLessThan(script.indexOf(spool))
+  }
+}
+
+function runPosixHook(
+  script: string,
+  grokHookEvent: string
+): {
+  curlCalled: boolean
+  stdout: string
+} {
+  const dir = mkdtempSync(join(tmpdir(), 'orca-grok-replay-'))
+  const scriptPath = join(dir, 'hook.sh')
+  const curlPath = join(dir, 'curl')
+  const curlLog = join(dir, 'curl.log')
+  try {
+    writeFileSync(scriptPath, script)
+    writeFileSync(
+      curlPath,
+      '#!/bin/sh\n{ command -p cat 2>/dev/null || cat; } >/dev/null\nprintf "called\\n" >> "$CURL_LOG"\n'
+    )
+    chmodSync(scriptPath, 0o755)
+    chmodSync(curlPath, 0o755)
+
+    const result = spawnSync('/bin/sh', [scriptPath], {
+      encoding: 'utf8',
+      input: '{"hook_event_name":"Stop"}',
+      env: {
+        ...process.env,
+        PATH: `${dir}:${process.env.PATH ?? ''}`,
+        CURL_LOG: curlLog,
+        GROK_HOOK_EVENT: grokHookEvent,
+        ORCA_AGENT_HOOK_ENDPOINT: '',
+        ORCA_AGENT_HOOK_PORT: '1234',
+        ORCA_AGENT_HOOK_TOKEN: 'token',
+        ORCA_PANE_KEY: 'tab:leaf'
+      }
+    })
+
+    expect(result.error).toBeUndefined()
+    expect(result.status).toBe(0)
+    return { curlCalled: existsSync(curlLog), stdout: result.stdout }
+  } finally {
+    rmSync(dir, { recursive: true, force: true })
+  }
+}
+
+describe('Grok vendor hook replay guard', () => {
+  it('precedes spooling and HTTP in the generated POSIX Claude and Cursor scripts', () => {
+    const claude = getClaudeManagedScript('posix', CLAUDE_SCRIPT_OPTIONS)
+    const cursor = getCursorManagedScript('posix')
+
+    expectGuardBeforeTransport(claude, POSIX_GROK_GUARD, 'printf "{}\\n"', 'spool_hook_event')
+    expectGuardBeforeTransport(cursor, POSIX_GROK_GUARD, 'printf "{}\\n"', 'spool_hook_event')
+  })
+
+  it('precedes HTTP while preserving fail-open output in generated Windows scripts', () => {
+    const { claude, cursor } = withPlatform('win32', () => ({
+      claude: getClaudeManagedScript('local', CLAUDE_SCRIPT_OPTIONS),
+      cursor: getCursorManagedScript('local')
+    }))
+
+    expectGuardBeforeTransport(claude, WINDOWS_GROK_GUARD, 'echo {}')
+    expectGuardBeforeTransport(cursor, WINDOWS_GROK_GUARD, '(echo {})')
+    const backgroundWorkerGuardIndex = claude.indexOf('CLAUDE_JOB_DIR')
+    expect(backgroundWorkerGuardIndex).toBeGreaterThan(-1)
+    expect(backgroundWorkerGuardIndex).toBeLessThan(claude.indexOf(WINDOWS_GROK_GUARD))
+  })
+
+  it.skipIf(process.platform === 'win32')(
+    'drops Grok-replayed hooks without suppressing their protocol response',
+    () => {
+      for (const script of [
+        getClaudeManagedScript('posix', CLAUDE_SCRIPT_OPTIONS),
+        getCursorManagedScript('posix')
+      ]) {
+        const result = runPosixHook(script, 'Stop')
+        expect(result.curlCalled).toBe(false)
+        expect(result.stdout).toBe('{}\n')
+      }
+    }
+  )
+
+  it.skipIf(process.platform === 'win32')('leaves non-Grok hook delivery unchanged', () => {
+    for (const script of [
+      getClaudeManagedScript('posix', CLAUDE_SCRIPT_OPTIONS),
+      getCursorManagedScript('posix')
+    ]) {
+      const result = runPosixHook(script, '')
+      expect(result.curlCalled).toBe(true)
+      expect(result.stdout).toBe('{}\n')
+    }
+  })
+})