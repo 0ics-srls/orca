paidaxingyo666/Manta

feat(session-search): resolve Workspace and Project scope on the host (#21509)
* refactor(session-search): move the AI Vault project key to shared

The host must spell a project key exactly as the client does, so the two
sides share one function instead of two copies that can drift.

* feat(session-search): add a scope identity to the search request

The panel cannot keep translating a project into one path per worktree: a
repo with 580 of them exceeds the 64-path cap and the search fails outright.
The request now carries the scope's identity instead, and a host acknowledges
the scope it resolved so a client can tell a scoped answer from an old host's
unscoped one.

* feat(session-search): resolve a scope identity on the host that answers

Every entry point already funnels into searchSessionService, so the identity
becomes paths there once: native, WSL, SSH and relay hosts cannot disagree.
A host that does not know the workspace or project answers scope-unknown
rather than widening the search to everything it has.

* test(session-search): pin how a host resolves a scope identity

Covers prior paths, a workspace another now claims, folder workspaces, a
custom worktree base path, flat placement where the global root belongs to
every project, and the 580-worktree fold the panel's path list could not do.

* fix(session-search): type the scope store by what the catalog reads

A full Repo/Project/ProjectHostSetup requirement forced test stores to stand
up rows the catalog never looks at.

* feat(session-search): send the scope identity from the panel

Workspace and Project name what to narrow to; All sends nothing. A host that
answers a scoped search without acknowledging it is reported as needing an
update, and none of its hits are shown, because they are not this scope's.

* test(session-search): pin the new-client-against-old-host skew

An old host strips the identity and answers with every session it has, and
the answer is well-formed. The missing acknowledgement is t

src/main/ai-vault-search/session-search-scope-entry-points.test.ts
@@ -1,0 +1,116 @@
+import { afterEach, describe, expect, it } from 'vitest'
+import { AiVaultHandler } from '../../relay/ai-vault-handler'
+import type { RelayDispatcher } from '../../relay/dispatcher'
+import { createSessionSearchClient } from '../../shared/ai-vault-search-client'
+import { fakeSearchService } from '../../shared/ai-vault-search-test-fixture'
+import { RpcDispatcher } from '../runtime/rpc/dispatcher'
+import { MantaRuntimeService } from '../runtime/manta-runtime'
+import { AI_VAULT_METHODS } from '../runtime/rpc/methods/ai-vault'
+import {
+  installSessionSearchScopeCatalogSource,
+  resetSessionSearchScopeCatalogForTests
+} from './session-search-scope-catalog'
+import { searchSessionService, setSessionSearchService } from './session-search-service-registry'
+
+const CATALOG = {
+  repos: [{ id: 'repo-1', path: '/work/app' }],
+  projects: [],
+  projectHostSetups: [],
+  worktreeMeta: {},
+  settings: { workspaceDir: '/home/me/manta/workspaces', nestWorkspaces: true }
+}
+const WITHIN = { kind: 'workspace', worktreeId: 'repo-1::/work/app' } as const
+
+afterEach(() => {
+  setSessionSearchService(null)
+  resetSessionSearchScopeCatalogForTests()
+})
+
+function relayHandler(): (params: Record<string, unknown>) => Promise<unknown> {
+  const handlers = new Map<string, (params: Record<string, unknown>) => Promise<unknown>>()
+  const dispatcher = {
+    onRequest: (method: string, handler: (params: Record<string, unknown>) => Promise<unknown>) => {
+      handlers.set(method, handler)
+    }
+  }
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: the handler only calls `onRequest`, and the test asserts the registration it makes.
+  new AiVaultHandler(dispatcher as unknown as RelayDispatcher)
+  const search = handlers.get('aiVault.searchSessions')
+  if (!search) {
+    throw new Error('relay registered no session search handler')
+  }
+  return search
+}
+
+describe('every search entry point carries the scope identity through', () => {
+  it('resolves over the in-process IPC entry point', async () => {
+    const service = fakeSearchService()
+    setSessionSearchService(service)
+    installSessionSearchScopeCatalogSource(() => CATALOG)
+    await searchSessionService({ query: 'needle', within: WITHIN }, 'ipc')
+    expect(service.search).toHaveBeenCalledWith(expect.anything(), {
+      kind: 'resolved',
+      paths: ['/work/app']
+    })
+  })
+
+  it('resolves over the runtime RPC method', async () => {
+    const service = fakeSearchService()
+    setSessionSearchService(service)
+    installSessionSearchScopeCatalogSource(() => CATALOG)
+    const rpc = new RpcDispatcher({
+      runtime: new MantaRuntimeService(),
+      methods: AI_VAULT_METHODS
+    })
+    await rpc.dispatch({
+      id: 'search-1',
+      authToken: 'test',
+      method: 'aiVault.searchSessions',
+      params: { query: 'needle', within: WITHIN }
+    })
+    expect(service.search).toHaveBeenCalledWith(expect.anything(), {
+      kind: 'resolved',
+      paths: ['/work/app']
+    })
+  })
+
+  it('resolves over the relay entry point', async () => {
+    const service = fakeSearchService()
+    setSessionSearchService(service)
+    installSessionSearchScopeCatalogSource(() => CATALOG)
+    await relayHandler()({ query: 'needle', within: WITHIN })
+    expect(service.search).toHaveBeenCalledWith(expect.anything(), {
+      kind: 'resolved',
+      paths: ['/work/app']
+    })
+  })
+
+  it('hands the relay’s own verdict down, that host carrying no repo catalog', async () => {
+    const service = fakeSearchService()
+    setSessionSearchService(service)
+    await relayHandler()({ query: 'needle', within: WITHIN })
+    expect(service.search).toHaveBeenCalledWith(
+      { query: 'needle', limit: 20 },
+      {
+        kind: 'unknown'
+      }
+    )
+  })
+})
+
+describe('the shared remote client', () => {
+  it('carries the identity out across a transport', async () => {
+    const service = fakeSearchService()
+    setSessionSearchService(service)
+    installSessionSearchScopeCatalogSource(() => CATALOG)
+    const client = createSessionSearchClient(
+      (_method, params) => searchSessionService(params, 'relay'),
+      'relay'
+    )
+    await client.searchSessions({ query: 'needle', within: WITHIN })
+    expect(service.search).toHaveBeenCalledWith(expect.anything(), {
+      kind: 'resolved',
+      paths: ['/work/app']
+    })
+  })
+})

src/main/ai-vault-search/session-search-scope-paths.ts
@@ -1,0 +1,71 @@
+import {
+  getRuntimePathBasename,
+  isPathInsideOrEqual,
+  isRuntimePathAbsolute,
+  normalizeRuntimePathForComparison,
+  resolveRuntimePath
+} from '../../shared/cross-platform-path'
+import { isFolderRepo } from '../../shared/repo-kind'
+import { resolveConfiguredWorktreeBasePaths } from '../../shared/worktree/configured-worktree-base-path'
+import { buildKnownMantaWorkspaceLayouts } from '../../shared/worktree/ownership'
+import type { SessionSearchScopeCatalog } from './session-search-scope-catalog'
+
+type ScopeRepo = SessionSearchScopeCatalog['repos'][number]
+
+/** Absolute paths in insertion order, deduplicated by the comparison key. */
+export class ScopePathSet {
+  private readonly seen = new Set<string>()
+  private readonly entries: string[] = []
+
+  add(value: string | null | undefined): void {
+    const trimmed = value?.trim()
+    if (!trimmed || !isRuntimePathAbsolute(trimmed)) {
+      return
+    }
+    const key = normalizeRuntimePathForComparison(trimmed)
+    if (this.seen.has(key)) {
+      return
+    }
+    this.seen.add(key)
+    this.entries.push(trimmed)
+  }
+
+  // The index matches by prefix, so a contained path is one more SQL range for
+  // no extra rows.
+  folded(): string[] {
+    return this.entries.filter(
+      (candidate) =>
+        !this.entries.some((other) => other !== candidate && isPathInsideOrEqual(other, candidate))
+    )
+  }
+}
+
+/**
+ * The directories Manta creates this repo's worktrees in, past and present, where
+ * such a directory belongs to this repo alone.
+ *
+ * A global root counts only under nesting: flat placement makes it every
+ * project's, and claiming it would widen a project search to the whole machine.
+ * An empty result costs only the folding — every registered worktree is still listed.
+ */
+export function managedWorktreeDirectories(
+  repo: ScopeRepo,
+  settings: SessionSearchScopeCatalog['settings']
+): string[] {
+  if (isFolderRepo(repo)) {
+    return []
+  }
+  const configured = new Set(
+    resolveConfiguredWorktreeBasePaths(repo).map(normalizeRuntimePathForComparison)
+  )
+  const repoName = getRuntimePathBasename(repo.path).replace(/\.git$/, '')
+  const directories: string[] = []
+  for (const layout of buildKnownMantaWorkspaceLayouts(settings, repo)) {
+    if (configured.has(normalizeRuntimePathForComparison(layout.path))) {
+      directories.push(layout.path)
+    } else if (layout.nestWorkspaces && repoName) {
+      directories.push(resolveRuntimePath(layout.path, repoName))
+    }
+  }
+  return directories
+}

src/main/ai-vault-search/session-search-store-scope-catalog.test.ts
@@ -1,0 +1,60 @@
+import { describe, expect, it } from 'vitest'
+import type { WorktreeMeta } from '../../shared/worktree/meta-types'
+import { sessionSearchScopeCatalogFromStore } from './session-search-store-scope-catalog'
+
+function meta(hostId: WorktreeMeta['hostId']): WorktreeMeta {
+  return {
+    hostId,
+    displayName: '',
+    comment: '',
+    linkedIssue: null,
+    linkedPR: null,
+    linkedLinearIssue: null,
+    isArchived: false,
+    isUnread: false,
+    isPinned: false,
+    sortOrder: 0,
+    lastActivityAt: 0
+  }
+}
+
+function store() {
+  return {
+    getRepos: () => [
+      { id: 'local-repo', path: '/work/app' },
+      { id: 'ssh-repo', path: '/srv/app', connectionId: 'box' }
+    ],
+    getProjects: () => [{ id: 'proj-1', sourceRepoIds: ['local-repo'] }],
+    getProjectHostSetups: () => [
+      { projectId: 'proj-1', hostId: 'local' as const, repoId: 'local-repo', path: '/work/app' },
+      { projectId: 'proj-1', hostId: 'ssh:box' as const, repoId: 'ssh-repo', path: '/srv/app' }
+    ],
+    getAllWorktreeMeta: () => ({
+      'local-repo::/work/one': meta('local'),
+      'ssh-repo::/srv/one': meta('ssh:box')
+    }),
+    getSettings: () => ({ workspaceDir: '/home/me/manta/workspaces', nestWorkspaces: true })
+  }
+}
+
+describe('scope catalog from the profile store', () => {
+  it('keeps only the rows the addressed execution host owns', () => {
+    const local = sessionSearchScopeCatalogFromStore(store(), 'local')
+    expect(local.repos.map((repo) => repo.id)).toEqual(['local-repo'])
+    expect(local.projectHostSetups.map((setup) => setup.repoId)).toEqual(['local-repo'])
+    expect(Object.keys(local.worktreeMeta)).toEqual(['local-repo::/work/one'])
+  })
+
+  it('answers for an SSH host from that host’s own rows, not the desktop’s', () => {
+    const remote = sessionSearchScopeCatalogFromStore(store(), 'ssh:box')
+    expect(remote.repos.map((repo) => repo.id)).toEqual(['ssh-repo'])
+    expect(Object.keys(remote.worktreeMeta)).toEqual(['ssh-repo::/srv/one'])
+  })
+
+  it('carries the placement settings a managed worktree directory is derived from', () => {
+    expect(sessionSearchScopeCatalogFromStore(store(), 'local').settings).toEqual({
+      workspaceDir: '/home/me/manta/workspaces',
+      nestWorkspaces: true
+    })
+  })
+})

0ics-srls/orca

(cherry picked from commit 73a58bd21ac50a23922e6b2f482c003ebef552b7)

src/main/ai-vault-search/session-search-scope-entry-points.test.ts
@@ -1,0 +1,116 @@
+import { afterEach, describe, expect, it } from 'vitest'
+import { AiVaultHandler } from '../../relay/ai-vault-handler'
+import type { RelayDispatcher } from '../../relay/dispatcher'
+import { createSessionSearchClient } from '../../shared/ai-vault-search-client'
+import { fakeSearchService } from '../../shared/ai-vault-search-test-fixture'
+import { RpcDispatcher } from '../runtime/rpc/dispatcher'
+import { OrcaRuntimeService } from '../runtime/orca-runtime'
+import { AI_VAULT_METHODS } from '../runtime/rpc/methods/ai-vault'
+import {
+  installSessionSearchScopeCatalogSource,
+  resetSessionSearchScopeCatalogForTests
+} from './session-search-scope-catalog'
+import { searchSessionService, setSessionSearchService } from './session-search-service-registry'
+
+const CATALOG = {
+  repos: [{ id: 'repo-1', path: '/work/app' }],
+  projects: [],
+  projectHostSetups: [],
+  worktreeMeta: {},
+  settings: { workspaceDir: '/home/me/orca/workspaces', nestWorkspaces: true }
+}
+const WITHIN = { kind: 'workspace', worktreeId: 'repo-1::/work/app' } as const
+
+afterEach(() => {
+  setSessionSearchService(null)
+  resetSessionSearchScopeCatalogForTests()
+})
+
+function relayHandler(): (params: Record<string, unknown>) => Promise<unknown> {
+  const handlers = new Map<string, (params: Record<string, unknown>) => Promise<unknown>>()
+  const dispatcher = {
+    onRequest: (method: string, handler: (params: Record<string, unknown>) => Promise<unknown>) => {
+      handlers.set(method, handler)
+    }
+  }
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: the handler only calls `onRequest`, and the test asserts the registration it makes.
+  new AiVaultHandler(dispatcher as unknown as RelayDispatcher)
+  const search = handlers.get('aiVault.searchSessions')
+  if (!search) {
+    throw new Error('relay registered no session search handler')
+  }
+  return search
+}
+
+describe('every search entry point carries the scope identity through', () => {
+  it('resolves over the in-process IPC entry point', async () => {
+    const service = fakeSearchService()
+    setSessionSearchService(service)
+    installSessionSearchScopeCatalogSource(() => CATALOG)
+    await searchSessionService({ query: 'needle', within: WITHIN }, 'ipc')
+    expect(service.search).toHaveBeenCalledWith(expect.anything(), {
+      kind: 'resolved',
+      paths: ['/work/app']
+    })
+  })
+
+  it('resolves over the runtime RPC method', async () => {
+    const service = fakeSearchService()
+    setSessionSearchService(service)
+    installSessionSearchScopeCatalogSource(() => CATALOG)
+    const rpc = new RpcDispatcher({
+      runtime: new OrcaRuntimeService(),
+      methods: AI_VAULT_METHODS
+    })
+    await rpc.dispatch({
+      id: 'search-1',
+      authToken: 'test',
+      method: 'aiVault.searchSessions',
+      params: { query: 'needle', within: WITHIN }
+    })
+    expect(service.search).toHaveBeenCalledWith(expect.anything(), {
+      kind: 'resolved',
+      paths: ['/work/app']
+    })
+  })
+
+  it('resolves over the relay entry point', async () => {
+    const service = fakeSearchService()
+    setSessionSearchService(service)
+    installSessionSearchScopeCatalogSource(() => CATALOG)
+    await relayHandler()({ query: 'needle', within: WITHIN })
+    expect(service.search).toHaveBeenCalledWith(expect.anything(), {
+      kind: 'resolved',
+      paths: ['/work/app']
+    })
+  })
+
+  it('hands the relay’s own verdict down, that host carrying no repo catalog', async () => {
+    const service = fakeSearchService()
+    setSessionSearchService(service)
+    await relayHandler()({ query: 'needle', within: WITHIN })
+    expect(service.search).toHaveBeenCalledWith(
+      { query: 'needle', limit: 20 },
+      {
+        kind: 'unknown'
+      }
+    )
+  })
+})
+
+describe('the shared remote client', () => {
+  it('carries the identity out across a transport', async () => {
+    const service = fakeSearchService()
+    setSessionSearchService(service)
+    installSessionSearchScopeCatalogSource(() => CATALOG)
+    const client = createSessionSearchClient(
+      (_method, params) => searchSessionService(params, 'relay'),
+      'relay'
+    )
+    await client.searchSessions({ query: 'needle', within: WITHIN })
+    expect(service.search).toHaveBeenCalledWith(expect.anything(), {
+      kind: 'resolved',
+      paths: ['/work/app']
+    })
+  })
+})

src/main/ai-vault-search/session-search-scope-paths.ts
@@ -1,0 +1,71 @@
+import {
+  getRuntimePathBasename,
+  isPathInsideOrEqual,
+  isRuntimePathAbsolute,
+  normalizeRuntimePathForComparison,
+  resolveRuntimePath
+} from '../../shared/cross-platform-path'
+import { isFolderRepo } from '../../shared/repo-kind'
+import { resolveConfiguredWorktreeBasePaths } from '../../shared/worktree/configured-worktree-base-path'
+import { buildKnownOrcaWorkspaceLayouts } from '../../shared/worktree/ownership'
+import type { SessionSearchScopeCatalog } from './session-search-scope-catalog'
+
+type ScopeRepo = SessionSearchScopeCatalog['repos'][number]
+
+/** Absolute paths in insertion order, deduplicated by the comparison key. */
+export class ScopePathSet {
+  private readonly seen = new Set<string>()
+  private readonly entries: string[] = []
+
+  add(value: string | null | undefined): void {
+    const trimmed = value?.trim()
+    if (!trimmed || !isRuntimePathAbsolute(trimmed)) {
+      return
+    }
+    const key = normalizeRuntimePathForComparison(trimmed)
+    if (this.seen.has(key)) {
+      return
+    }
+    this.seen.add(key)
+    this.entries.push(trimmed)
+  }
+
+  // The index matches by prefix, so a contained path is one more SQL range for
+  // no extra rows.
+  folded(): string[] {
+    return this.entries.filter(
+      (candidate) =>
+        !this.entries.some((other) => other !== candidate && isPathInsideOrEqual(other, candidate))
+    )
+  }
+}
+
+/**
+ * The directories Orca creates this repo's worktrees in, past and present, where
+ * such a directory belongs to this repo alone.
+ *
+ * A global root counts only under nesting: flat placement makes it every
+ * project's, and claiming it would widen a project search to the whole machine.
+ * An empty result costs only the folding — every registered worktree is still listed.
+ */
+export function managedWorktreeDirectories(
+  repo: ScopeRepo,
+  settings: SessionSearchScopeCatalog['settings']
+): string[] {
+  if (isFolderRepo(repo)) {
+    return []
+  }
+  const configured = new Set(
+    resolveConfiguredWorktreeBasePaths(repo).map(normalizeRuntimePathForComparison)
+  )
+  const repoName = getRuntimePathBasename(repo.path).replace(/\.git$/, '')
+  const directories: string[] = []
+  for (const layout of buildKnownOrcaWorkspaceLayouts(settings, repo)) {
+    if (configured.has(normalizeRuntimePathForComparison(layout.path))) {
+      directories.push(layout.path)
+    } else if (layout.nestWorkspaces && repoName) {
+      directories.push(resolveRuntimePath(layout.path, repoName))
+    }
+  }
+  return directories
+}

src/main/ai-vault-search/session-search-store-scope-catalog.test.ts
@@ -1,0 +1,60 @@
+import { describe, expect, it } from 'vitest'
+import type { WorktreeMeta } from '../../shared/worktree/meta-types'
+import { sessionSearchScopeCatalogFromStore } from './session-search-store-scope-catalog'
+
+function meta(hostId: WorktreeMeta['hostId']): WorktreeMeta {
+  return {
+    hostId,
+    displayName: '',
+    comment: '',
+    linkedIssue: null,
+    linkedPR: null,
+    linkedLinearIssue: null,
+    isArchived: false,
+    isUnread: false,
+    isPinned: false,
+    sortOrder: 0,
+    lastActivityAt: 0
+  }
+}
+
+function store() {
+  return {
+    getRepos: () => [
+      { id: 'local-repo', path: '/work/app' },
+      { id: 'ssh-repo', path: '/srv/app', connectionId: 'box' }
+    ],
+    getProjects: () => [{ id: 'proj-1', sourceRepoIds: ['local-repo'] }],
+    getProjectHostSetups: () => [
+      { projectId: 'proj-1', hostId: 'local' as const, repoId: 'local-repo', path: '/work/app' },
+      { projectId: 'proj-1', hostId: 'ssh:box' as const, repoId: 'ssh-repo', path: '/srv/app' }
+    ],
+    getAllWorktreeMeta: () => ({
+      'local-repo::/work/one': meta('local'),
+      'ssh-repo::/srv/one': meta('ssh:box')
+    }),
+    getSettings: () => ({ workspaceDir: '/home/me/orca/workspaces', nestWorkspaces: true })
+  }
+}
+
+describe('scope catalog from the profile store', () => {
+  it('keeps only the rows the addressed execution host owns', () => {
+    const local = sessionSearchScopeCatalogFromStore(store(), 'local')
+    expect(local.repos.map((repo) => repo.id)).toEqual(['local-repo'])
+    expect(local.projectHostSetups.map((setup) => setup.repoId)).toEqual(['local-repo'])
+    expect(Object.keys(local.worktreeMeta)).toEqual(['local-repo::/work/one'])
+  })
+
+  it('answers for an SSH host from that host’s own rows, not the desktop’s', () => {
+    const remote = sessionSearchScopeCatalogFromStore(store(), 'ssh:box')
+    expect(remote.repos.map((repo) => repo.id)).toEqual(['ssh-repo'])
+    expect(Object.keys(remote.worktreeMeta)).toEqual(['ssh-repo::/srv/one'])
+  })
+
+  it('carries the placement settings a managed worktree directory is derived from', () => {
+    expect(sessionSearchScopeCatalogFromStore(store(), 'local').settings).toEqual({
+      workspaceDir: '/home/me/orca/workspaces',
+      nestWorkspaces: true
+    })
+  })
+})